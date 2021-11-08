c4eb96a69d8635c08cf0ecd5653e65fc55f006b8
aniketm32012 <92517637+aniketm32012@users.noreply.github.com>
2021-11-08 18:09:33 +0530

    Update Apex.py

--- Apex.py
+++ Apex.py
@@ -8,7 +8,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl('http://google.com'))
+        self.browser.setUrl(QUrl('https://www.youtube.com/'))
         self.setCentralWidget(self.browser)
         self.showMaximized()
 
